e434760a7fa24aea5e4f0b6922ee10d30cb0f606
Paul Northrop <p.northrop@ucl.ac.uk>
2019-09-29 22:46:42 +0100

    Tidy

--- tests/testthat/test-no_torus.R
+++ tests/testthat/test-no_torus.R
@@ -8,16 +8,13 @@ got_RANN <- requireNamespace("RANN", quietly = TRUE)
 if (got_RANN) {
   set.seed(20092019)
   # Example from the RANN:nn2 documentation
-  x1 <- runif(100, 0, 2*pi)
+  x1 <- runif(100, 0, 2 * pi)
   x2 <- runif(100, 0,3)
   DATA <- data.frame(x1, x2)
   # Use RANN::nn2 directly
   nearest <- RANN::nn2(DATA,DATA)
   # Use donut::nnt
   res <- nnt(DATA, DATA)
-  res$data <- NULL
-  res$call <- NULL
-  res$query <- NULL
   test_that("RANN: no wrapping, nn.idx", {
     testthat::expect_equal(nearest$nn.idx, res$nn.idx)
   })
