6adaecf875418d8c787ee45a663470c2f8213c88
Paul Northrop <p.northrop@ucl.ac.uk>
2019-09-29 23:12:16 +0100

    Add test

---
+++ tests/testthat/test-plot.R
@@ -0,0 +1,28 @@
+context("Plot method")
+
+# Check that RANN is available
+got_RANN <- requireNamespace("RANN", quietly = TRUE)
+
+if (got_RANN) {
+  set.seed(29092019)
+  x1 <- runif(100, 0, 2 * pi)
+  x2 <- runif(100, 0, 3)
+  x3 <- runif(100, -1, 1)
+
+  # 3D
+  x <- cbind(x1, x2, x3)
+  res <- nnt(x, x, torus = 1, ranges = c(0, 2 * pi), method = 1)
+  test_that("RANN: repeats 2D", {
+    the_error <- "The plot method works for up to 2 covariates only"
+    testthat::expect_error(plot(res), the_error)
+  })
+
+  # 2D
+  x <- cbind(x1, x2)
+  ranges <- matrix(c(0, 0, 2 * pi, 3), 2, 2)
+  query <- rbind(c(6, 0.1), c(3, 3))
+  res <- nnt(x, query, torus = 1:2, ranges = ranges, method = 1)
+  test_that("RANN: repeats 2D", {
+    testthat::expect_equal(plot(res), NULL)
+  })
+}
