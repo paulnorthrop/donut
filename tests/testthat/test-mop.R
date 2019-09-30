37b429f3fb5a0f98ed502cc14c5726b633c436d2
Paul Northrop <p.northrop@ucl.ac.uk>
2019-09-30 16:29:27 +0100

    Tidy

--- tests/testthat/test-mop.R
+++ tests/testthat/test-mop.R
@@ -15,21 +15,4 @@ if (got_RANN) {
     the_error <- "ranges must be supplied"
     testthat::expect_error(nnt(x, x, torus = 1, method = 1), the_error)
   })
-
-  # 2D
-  x <- cbind(x1, x2)
-  ranges <- matrix(c(0, 0, 2 * pi, 3), 2, 2)
-  query <- rbind(c(6, 0.1), c(3, 3))
-  res <- nnt(x, query, torus = 1:2, ranges = ranges, method = 1)
-  test_that("RANN: plot 2D", {
-    testthat::expect_equal(plot(res), NULL)
-  })
-
-  # 1D
-  ranges <- c(0, 2 * pi)
-  query <- c(6, 0.1)
-  res <- nnt(x1, query, torus = 1, ranges = ranges, method = 1)
-  test_that("RANN: plot 1D", {
-    testthat::expect_equal(plot(res), NULL)
-  })
 }
