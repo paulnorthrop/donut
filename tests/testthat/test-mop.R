7ff5c3b23a88b8bb2ccf698a698d3dbdbcd3ad86
Paul Northrop <p.northrop@ucl.ac.uk>
2019-09-30 16:50:59 +0100

    Add tests

--- tests/testthat/test-mop.R
+++ tests/testthat/test-mop.R
@@ -15,4 +15,30 @@ if (got_RANN) {
     the_error <- "ranges must be supplied"
     testthat::expect_error(nnt(x, x, torus = 1, method = 1), the_error)
   })
+
+  # Call nnt() with non-numeric torus
+  test_that("nnt: no ranges", {
+    the_error <- "'torus' must be a non-empty numeric vector"
+    testthat::expect_error(nnt(x, x, torus = "1", ranges = c(0, 1),
+                               method = 1), the_error)
+  })
+
+  # Call nnt() with a torus that is not in {1, ..., ncol(data)}
+  test_that("nnt: no ranges", {
+    testthat::expect_error(nnt(x, x, torus = c(1:2, 5), ranges = c(0, 1),
+                               method = 1))
+  })
+
+  # Call nnt() with a ranges that is not consistent with torus
+  test_that("nnt: no ranges", {
+    the_error <- "ranges not consistent with length(torus)"
+    testthat::expect_error(nnt(x, x, torus = 1:2, ranges = c(0, 1),
+                               method = 1))
+  })
+  # Call nnt() with an inappropriate value of method
+  test_that("nnt: no ranges", {
+    the_error <- "method must be equal to 1 or 2"
+    testthat::expect_error(nnt(x, x, torus = 1, ranges = c(0, 1),
+                               method = 3))
+  })
 }
--- tests/testthat/test-no_torus.R
+++ tests/testthat/test-no_torus.R
@@ -1,20 +1,19 @@
 context("No torus")
 
+set.seed(20092019)
+# Example from the RANN:nn2 documentation
+x1 <- runif(100, 0, 2 * pi)
+x2 <- runif(100, 0,3)
+DATA <- data.frame(x1, x2)
+
 # Check that RANN is available
 got_RANN <- requireNamespace("RANN", quietly = TRUE)
-
 # No wrapping
-
 if (got_RANN) {
-  set.seed(20092019)
-  # Example from the RANN:nn2 documentation
-  x1 <- runif(100, 0, 2 * pi)
-  x2 <- runif(100, 0,3)
-  DATA <- data.frame(x1, x2)
-  # Use RANN::nn2 directly
+  # Use RANN::nn2 directly (this is the default fn)
   nearest <- RANN::nn2(DATA,DATA)
   # Use donut::nnt
-  res <- nnt(DATA, DATA)
+  res <- nnt(DATA, DATA, fn = RANN::nn2)
   test_that("RANN: no wrapping, nn.idx", {
     testthat::expect_equal(nearest$nn.idx, res$nn.idx)
   })
@@ -23,3 +22,34 @@ if (got_RANN) {
   })
 }
 
+# Check that RANN.L1 is available
+got_RANN.L1 <- requireNamespace("RANN.L1", quietly = TRUE)
+# No wrapping
+if (got_RANN.L1) {
+  # Use RANN.L1::nn2 directly
+  nearest <- RANN.L1::nn2(DATA,DATA)
+  # Use donut::nnt
+  res <- nnt(DATA, DATA, fn = RANN.L1::nn2)
+  test_that("RANN.L1: no wrapping, nn.idx", {
+    testthat::expect_equal(nearest$nn.idx, res$nn.idx)
+  })
+  test_that("RANN.L1: no wrapping, nn.dists", {
+    testthat::expect_equal(nearest$nn.idx, res$nn.idx)
+  })
+}
+
+# Check that nabor is available
+got_nabor <- requireNamespace("nabor", quietly = TRUE)
+# No wrapping
+if (got_nabor) {
+  # Use nabor::knn directly
+  nearest <- nabor::knn(DATA,DATA, k = 10)
+  # Use donut::nnt
+  res <- nnt(DATA, DATA, fn = nabor::knn)
+  test_that("nabor: no wrapping, nn.idx", {
+    testthat::expect_equal(nearest$nn.idx, res$nn.idx)
+  })
+  test_that("nabor: no wrapping, nn.dists", {
+    testthat::expect_equal(nearest$nn.idx, res$nn.idx)
+  })
+}
