da536de95f07fbcf958dfdb4d0083e12f02c3821
Paul Northrop <p.northrop@ucl.ac.uk>
2019-09-30 15:54:20 +0100

    Add test

--- tests/testthat/test-plot.R
+++ tests/testthat/test-plot.R
@@ -12,7 +12,7 @@ if (got_RANN) {
   # 3D
   x <- cbind(x1, x2, x3)
   res <- nnt(x, x, torus = 1, ranges = c(0, 2 * pi), method = 1)
-  test_that("RANN: repeats 2D", {
+  test_that("RANN: plot 3D", {
     the_error <- "The plot method works for up to 2 covariates only"
     testthat::expect_error(plot(res), the_error)
   })
@@ -22,7 +22,15 @@ if (got_RANN) {
   ranges <- matrix(c(0, 0, 2 * pi, 3), 2, 2)
   query <- rbind(c(6, 0.1), c(3, 3))
   res <- nnt(x, query, torus = 1:2, ranges = ranges, method = 1)
-  test_that("RANN: repeats 2D", {
+  test_that("RANN: plot 2D", {
+    testthat::expect_equal(plot(res), NULL)
+  })
+
+  # 1D
+  ranges <- c(0, 2 * pi)
+  query <- c(6, 0.1)
+  res <- nnt(x1, query, torus = 1, ranges = ranges, method = 1)
+  test_that("RANN: plot 1D", {
     testthat::expect_equal(plot(res), NULL)
   })
 }
